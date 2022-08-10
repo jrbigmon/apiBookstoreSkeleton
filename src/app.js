2111538377b74fcb374b01f21f9cd1554b55c718
jrbigmon <junioterriaga0@gmail.com>
2022-08-10 06:42:07 -0300

    Finalizado alterações no todo e entry point

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const app = express();
-const port = 3000;
 const BookStoreRouter = require('./routes/BookStoreRouter');
 const AuthRouter = require('./routes/AuthRouter');
+
+const app = express();
+const port = 3000;
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/bookstore/api', AuthRouter);
 app.use('/bookstore/api', BookStoreRouter);
-
 app.use((req, res) => {
     return res.status(400).json('Page not found!');
 });
+
 
 app.listen(port, () => {
     console.log('listening on port ' + port);
